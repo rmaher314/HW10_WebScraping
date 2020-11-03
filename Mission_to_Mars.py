e4f9dfbcf0fbaa1932a676daad94609a962c7edd
Regan <regan_maher@outlook.com>
2020-11-02 19:49:06 -0600

    Final Commit.

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -38,17 +38,30 @@ def scrape():
 
     time.sleep(1)
 
-    full_image_elem = feature.find_by_id('full_image')
-    full_image_elem.click()
+    full_image_button = feature.find_by_id("full_image")
+    full_image_button.click()
+
+    # Find "More Info" Button and Click It
+    feature.is_element_present_by_text("more info", wait_time=2)
+    more_info_element = feature.find_link_by_partial_text("more info")
+    more_info_element.click()
+# #Added this element in
+#     feature.is_element_present_by_text('more info', wait_time=1)
+#     more_info_elem = feature.links.find_by_partial_text('more info')
+#     more_info_elem.click()
+# #Added Element Complete
 
     html = feature.html
     soup = BeautifulSoup(html, "html.parser")
     try:
-        img_url_rel = soup.select_one('figure.lede a img').get("src")
-        mars_data["src"] = img_url_rel
+       #feature_img = soup.select_one("figure.lede a img").get("src")
+       feature_img = soup.find("img", class_="main_image")
     except AttributeError:
-        print("exception caught")
-
+       return None
+   # Use Base URL to Create Absolute URL
+    feature_img = f"https://www.jpl.nasa.gov{feature_img['src']}"
+    
+    mars_data["feature_img"] = feature_img
 #NASA Mars News
 
 #Collecting the mars news form nasa.gov.
@@ -88,17 +101,15 @@ def scrape():
     baseUrl = 'https://astrogeology.usgs.gov'
     
     hemisphere_info = []
-    imageUrl = ''
+    #imageUrl = ''
     counter = 0
     keyCounter = 0;    
 
-    print("starting loop")
     for a in soup.find_all('a', {'class':'itemLink'}):
         counter = counter + 1
         hemisphereurl = baseUrl + a['href']
         if counter % 2 == 0:
-            #print (hemisphereurl)
-            #imgUrl += hemisphereurl + " "
+            
 
             feature.visit(hemisphereurl)
             time.sleep(2)
@@ -106,33 +117,45 @@ def scrape():
             html = feature.html
             soup2 = BeautifulSoup(html, "html.parser")
             
+            hemi_info = {}
+
+
             hemisphere_name = soup2.find("h2", {"class":"title"}).text
-                    
+
+            hemi_info["title"] = hemisphere_name
+
+
+
             divs = feature.find_by_tag("img")#[class='wide-image']
             matches = soup2.find("img", {"class":"wide-image"}).get("src")
-            imageUrl = baseUrl + matches
-            print(hemisphere_name)
-            print(imageUrl)
-            name = "name"+ str(keyCounter)
-            link = "link"+str(keyCounter)
-            keyCounter = keyCounter +1
-            if len(hemisphere_info)==0:
-               hemisphere_info ={
-                name: hemisphere_name,
-                link: imageUrl
-            }
-            else:
-                hemisphere_info.update({
-                    name: hemisphere_name,
-                    link: imageUrl
-                })
-            print(hemisphere_info)   
+            
+            hemi_url = baseUrl + matches
+            hemi_info["url"] = hemi_url
+
+            hemisphere_info.append(hemi_info)
+
+            # print(hemisphere_name)
+            # print(imageUrl)
+            # name = "name"+ str(keyCounter)
+            # link = "link"+str(keyCounter)
+            # keyCounter = keyCounter +1
+            # if len(hemisphere_info)==0:
+            #    hemisphere_info ={
+            #     name: hemisphere_name,
+            #     link: imageUrl
+            # }
+            # else:
+            #     hemisphere_info.update({
+            #         name: hemisphere_name,
+            #         link: imageUrl
+            #     })
+            # print(hemisphere_info)   
             feature.back()
             time.sleep(2)
                 
     print("exited loop")           
-    mars_data["hemisphere_info"]=hemisphere_info
-    html = feature.html
-    soup = BeautifulSoup(html, "html.parser")
+    mars_data["hemisphere_info"] = hemisphere_info
+    # html = feature.html
+    # soup = BeautifulSoup(html, "html.parser")
     feature.quit()
     return mars_data
